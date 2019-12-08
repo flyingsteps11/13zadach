5d2f8984c61083c9301ae650b5a7c31654352b82
Mikhail Vovk <mvovk@mmtr.ru>
2019-12-08 22:19:07 +0300

    7zadanie

--- 7/index.js
+++ 7/index.js
@@ -1,11 +1,20 @@
 //Написать функцию, которая на вход принимает последовательность букв и ищет
 //пропущенные буквы. Если все буквы присутствуют в диапазоне, то возвращается
 //undefined. Например: fearNotLetter("abce") должна вернуть "d
-function fearNotLetter(str){
-    let arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    let letters = str.split("").sort();
-    let letters_arr_en = arr_en.slice(arr_en.indexOf(letters[0]), arr_en.indexOf(letters[letters.length-1]));
-    let res = letters_arr_en.filter(i=> letters.indexOf(i) < 0).join('');
-    return res ==''? undefined : res
+function fearNotLetter(str) {
+    let str1 = str.toLowerCase().split('');
+    let  arr = "";
+    if(str1[0]==="a"){
+        for(let i=0;i<=str1.length;i++){
+            if(i+1<str1.length){
+                num1 = parseInt(str1[i].charCodeAt(0)) + 1;
+                num2 = parseInt(str1[i+1].charCodeAt(0));
+                if(num1!=num2){
+                    arr = arr + String.fromCharCode(num1);
+                }
+            }
+        }
+        return arr;
+    }
 }
 module.exports = fearNotLetter;
